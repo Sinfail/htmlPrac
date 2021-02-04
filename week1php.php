6acd2c8a6520cc41c61cd3b6f7045f14076bd58d
Baz <megaplayz8@gmail.com>
2021-02-05 10:47:11 +1100

    initial commit (week 1)
    week 1 work required for vsv

---
+++ week1php.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+</head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Hello world php </title>
+
+<body>
+<?php
+echo "hello world";
+$bruh = "this is a variable test";
+
+echo $bruh;
+echo '<br>';
+
+?>
+</body>
+
+
+
+
+</html>
